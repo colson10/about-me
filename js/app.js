ee71259a02d39f73e60ab8789a86fc2cfd8ec813
Carl Olson <colson10@gmail.com>
2018-01-09 14:05:46 -0800

    first prompt added and verified that is it working

---
+++ js/app.js
@@ -0,0 +1,5 @@
+'use strict';
+
+var userName = prompt('Hi! I have a guessing game with questions about me. But first, what is your name?');
+console.log('For name, the user entered: ' + userName);
+
